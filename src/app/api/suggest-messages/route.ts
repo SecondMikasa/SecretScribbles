d6a21fc0aec88f058fcc400c01e97bf27c031ebe
Kumar Arnim <kumararnim1@vivaldi.net>
2025-04-23 23:38:41 +0530

    Fix suggest messages api endpoint

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,30 +1,34 @@
-import { openai } from '@ai-sdk/openai';
+import { mistral } from '@ai-sdk/mistral';
 import { streamText } from 'ai';
 import { APIError } from 'openai/error.mjs';
 
 // Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+export const maxDuration = 30
 
-// OPENAI_API_KEY will be used by default by Vercel AI SDK
+//NOTE: Mistral_API_KEY will be used from environment variables by default by Vercel AI SDK
 
-export async function POST(req: Request) {
+//Set the runtime to edge for best performance
+export const runtime = 'edge'
+
+const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
+
+export async function POST(request: Request) {
+
   try {
-    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
-
     const result = await streamText({
-      model: openai('gpt-3.5-turbo-instruct'),
+      model: mistral('mistral-large-latest'),
       maxTokens: 400,
-      prompt,
-    });
+      prompt: prompt,
+      temperature: 1
+    })
+    
+    return result.toAIStreamResponse()
 
-    return result.toAIStreamResponse();
-
-  } catch (error) {
+  }
+  catch (error) {
     if (error instanceof APIError) {
-
-      // OpenAI API error handling
-      const { name, status, headers, message } = error;
-
+      // API error handling
+      const { name, status, headers, message } = error
       return Response.json(
         {
           name,
@@ -37,9 +41,8 @@ export async function POST(req: Request) {
         }
       )
     }
-
     else {
-      console.error("An unexpected error occured: ", error)
+      console.error("An unexpected error occurred: ", error)
       throw error
     }
   }
